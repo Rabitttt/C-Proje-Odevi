ba63391b8b16969e6392ee818a80ce6084689ae1
Rabitttt <burakkaragol60@gmail.com>
2019-05-25 01:25:24 +0300

    Second Commit with No file and some functions

--- main.c
+++ main.c
@@ -8,9 +8,10 @@
 typedef struct Yolcu_Bilgileri
 {
   int Yas;
-  char Isim[30];
-  char SoyIsim[30];
-  int TC_kimlikNo;
+  char Ad[30];
+  char Soyad[30];
+  int koltuk_numarasi;
+
 
 }yolcu;
 
@@ -19,7 +20,13 @@ typedef struct BiletBilgisi
   int biletNO;
   char Kalkis_Sehri[30];
   char Varis_Sehri[30];
-  //TARÝH BÝLGÝLERÝ GÝRÝLECEK
+
+  int koltuklarDizisi[21][5];
+
+  int Gun;
+  int Ay;
+  int Yil;
+
 
   int yolcu_Sayisi;
 
@@ -38,8 +45,6 @@ void AnaMenu()
 
    Bilet *bilet;
 
-   bilet = (Bilet *)malloc(sizeof(Bilet)*1);
-
    int secim;
    printf("1-Bilet Al\n");
    printf("2-Bilet Guncelle\n");
@@ -52,7 +57,7 @@ void AnaMenu()
 
        if(secim==1)
        {
-         Bilet_Al(bilet);
+         Bilet_al(bilet);
        }
        else if(secim==2)
        {
@@ -75,30 +80,44 @@ void AnaMenu()
          AnaMenu();
        }
 }
-void Bilet_Al(Bilet *bilet)
+
+void Bilet_al(Bilet *bilet)
 {
 
-  int i,j;
+    bilet = (Bilet *)malloc(sizeof(Bilet)*1);
+    Koltuklar(bilet);
+
+    int i,j;
+
+    for(i=0;i<1;i++)
+    {
+        Sefer(bilet);
+        Tarih(bilet);
+
+        printf("Kac Tane Bilet Alacaginizi Seciniz: ");
+        scanf("%d",&bilet->yolcu_Sayisi);
+
+        bilet->Yolcular = (yolcu *)malloc((bilet->yolcu_Sayisi)*sizeof(yolcu));
+
+        for(j=0 ; j<(bilet+i)->yolcu_Sayisi ; j++)
+        {
+            printf("%d. yolcunun Adi : ");
+            scanf("%d", &((bilet)->Yolcular+j)->Ad);
+
+            printf("%d. yolcunun Soyadi : ");
+            scanf("%d", &((bilet)->Yolcular+j)->Soyad);
+
+            printf("%d. yolcunun Yasi: ");
+            scanf("%d", &((bilet)->Yolcular+j)->Yas);
+
+            koltukSecimi(bilet , j);
+        }
+    }
 
 
-  Sefer(bilet);
 
-
-  for(i=0;i<1;i++)
-  {
-
-
-
-
-
-   for(j=0;j<)
-   {
-
-   }
-
-  }
-   // BİLAT AL KISMINI SEFER KISMININ ALTINA TASI****************************************************************************
 }
+
 
 void Sefer(Bilet *bilet)
 {
@@ -111,61 +130,160 @@ void Sefer(Bilet *bilet)
     printf("4- Izmır");
     printf("5- Antalya");
 
-    int sayac=0;
-    int i=0;
     int kalkis_sehri;
     int varis_sehri;
-    int Farkli_sefer;
 
-    while(1==1)
+    printf("Kalkis Sehri Seciniz : ");
+    scanf("%d",&kalkis_sehri);
+
+    printf("Varis Sehri Seciniz : ");
+    scanf("%d",varis_sehri);
+
+
+    strcpy(Sehirler[kalkis_sehri-1],bilet->Kalkis_Sehri);
+    strcpy(Sehirler[varis_sehri-1],bilet->Varis_Sehri);
+
+}
+
+void Tarih(Bilet *bilet)
+{
+
+   int i;
+   char Aylar[12][40]={"Ocak","Subat","Mart","Nisan","Mayis","Haziran","Temmuz","Agustos","Eylul","Ekim","Kasim","Aralik"};
+   char Gunler[7][40]={"Pazartesi","Sali","Carsamba","Persembe","Cuma","Cumartesi","Pazar"};
+
+   int ay;
+   int gun;
+
+
+   for(i=0;i<12;i++)
+   {
+       printf("\n%d-) %s ",i+1,Aylar[i]);
+   }
+
+    printf("\nTarih Giriniz(2019 Yili Icerisinde)\n");
+    printf("\nAY(1-12): ");
+    scanf("%d",&ay);
+
+    if(0>ay || ay>12)
     {
-        sayac += 1;
-
-        bilet = (Bilet *)realloc(bilet,sizeof(Bilet)*sayac);
-
-        printf("Kalkis Sehri Seciniz : ");
-        scanf("%d",&kalkis_sehri);
-
-        printf("Varis Sehri Seciniz : ");
-        scanf("%d",&varis_sehri);
-
-
-
-        strcpy( (bilet+i)->Kalkis_Sehri, kalkis_sehri);
-        strcpy( (bilet+i)->Varis_Sehri,varis_sehri );
-
-
-        printf("Farkli Bir Sefer Olusturmak Ister misiniz:\n");
-        printf("1-Evet \n 2-Hayir");
-        scanf("%d",&Farkli_sefer);
-        if(Farkli_sefer==1)
-        {
-            system("CLS");
-        }
-        else if(Farkli_sefer==2)
-        {
-            break;
-        }
-        else
-        {
-         printf("Hatali Secim...")
-         break;
-        }
-
-
-        i += 1;
+        printf("Gecersiz Tarih\n");
+        system("CLS");
+        Tarih()
     }
 
+
+    printf("Ayin Kacinda Gideceginizi Seciniz (1-30): ");
+    scanf("%d",&bilet->Gun);
+
+    printf("Bilet Tarihi : %d.%d.%d ", bilet->Gun , bilet->Ay , 2019);
 
 
 
 }
 
 
-void Koltuklar()
+void Koltuklar(Bilet *bilet)
+{
+  int i,j,t=0;
+
+  for(i=0; i<21; i++)
+  {
+      for(j=0; j<5; j++)
+      {
+      if(j!=2)
+      {
+        bilet->koltuklarDizisi[i][j]=t+1;
+        t++;
+      }
+      else
+      {
+        bilet->koltuklarDizisi[i][j]= 32;
+      }
+
+  }
+  }
+}
+
+void koltukSecimi(Bilet *bilet , y)
 {
 
-  //KOLTUKLAR SECILIR
+int UcakSecilenKoltuk;
+int i,j,k;
+//************************************************DIZIYI YAZDIRMA
+
+for(i=0 ; i<y ; i++)
+{
+    for(j=0; j<21; j++)
+    {
+        for(k=0; k<5; k++)
+        {
+            if(k!=2)
+            {
+                printf("[%d]",bilet->koltuklarDizisi[j][k]);
+            }
+            else
+            {
+                printf("%c",bilet->koltuklarDizisi[j][k]);
+            }
+        }
+      printf("\n");
+    }
+   //****************************************************************
+
+
+      printf("Koltuk Seciniz: ");
+      scanf("%d",&UcakSecilenKoltuk);
+
+      if(UcakSecilenKoltuk<1 || UcakSecilenKoltuk>84)
+      {
+             printf("UZGUNUZ!! Sectiginiz Koltuk Bulunmamaktadir");
+             delay(1200);
+             system("CLS");
+             koltukSecimi(Bilet *bilet , y);
+      }
+
+
+
+        for(j=0; j<21; j++)
+            for(k=0; k<5; k++)
+            {
+              if( ((bilet)->Yolcular+y)->koltuk_numarasi == UcakSecilenKoltuk )
+              {
+                  printf("Secilen Koltuk Dolu Lutfen Farkli Bir koltuk Seciniz..");
+                  system("CLS");
+                  koltukSecimi(Bilet *bilet , y);
+              }
+              if( UcakSecilenKoltuk == bilet->koltuklarDizisi[i][j] )
+              {
+                  printf("Koltuk Secilmistir..");
+                  ((bilet)->Yolcular+y)->koltuk_numarasi = UcakSecilenKoltuk;
+                  bilet->koltuklarDizisi[i][j] = 0;
+              }
+
+
+            }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+void Ekleme()
+{
+
 }
 
 
@@ -183,3 +301,8 @@ void Fis_Bastir()
 {
 
 }
+
+
+
+
+
